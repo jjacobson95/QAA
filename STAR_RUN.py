98d630c3b63359e368251e5ab000206515c54b1d
Jeremy Jacobson <85139244+jjacobson95@users.noreply.github.com>
2021-09-08 09:45:53 -0700

    Delete STAR_RUN.py

--- STAR_RUN.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-/usr/bin/time -v STAR --runThreadN 8 --runMode genomeGenerate \
---genomeDir /projects/bgmp/jjacobso/bioinformatics/Bi623/Assignments/QAA/Alignment_stuff/Mus_musculus.GRCm39.dna.ens104.star_2.7.9a \
---genomeFastaFiles /projects/bgmp/jjacobso/bioinformatics/Bi623/Assignments/QAA/Alignment_stuff/Mus_musculus.GRCm39.dna.primary_assembly.fa \
---sjdbGTFfile /projects/bgmp/jjacobso/bioinformatics/Bi623/Assignments/QAA/Alignment_stuff/Mus_musculus.GRCm39.104.gtf
